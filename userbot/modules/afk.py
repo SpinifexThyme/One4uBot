e45b67344bfc0a4e6347bac55dc68467c9fec665
SpinifexThyme <68504371+SpinifexThyme@users.noreply.github.com>
2020-07-20 23:14:52 +0800

    Update afk.py

--- userbot/modules/afk.py
+++ userbot/modules/afk.py
@@ -164,8 +164,8 @@ async def mention_afk(mention):
                 afk_since = f"`{int(seconds)}s` yang lalu"
             if mention.sender_id not in USERS:
                 if AFKREASON:
-                    await mention.reply(str(choice(AFKSTR))) + (f"Gue OFF sejak {afk_since}.\
-                        \nKarena: `{AFKREASON}`")
+                    await mention.reply(str(choice(AFKSTR))) + (str(f"Gue OFF sejak {afk_since}.\
+                        \nKarena: `{AFKREASON}`"))
                 else:
                     await mention.reply(str(choice(AFKSTR)))
                 USERS.update({mention.sender_id: 1})
