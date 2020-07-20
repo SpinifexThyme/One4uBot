445159a4c969d09c3c0c248481fc5fa7a12dd4ee
SpinifexThyme <68504371+SpinifexThyme@users.noreply.github.com>
2020-07-20 21:12:41 +0800

    Update afk.py

--- userbot/modules/afk.py
+++ userbot/modules/afk.py
@@ -164,8 +164,7 @@ async def mention_afk(mention):
                 afk_since = f"`{int(seconds)}s` yang lalu"
             if mention.sender_id not in USERS:
                 if AFKREASON:
-                    await mention.reply(str(choice(AFKSTR)) f"\n\
-                        \nGue OFF sejak {afk_since}.\
+                    await mention.reply(f"\nGue OFF sejak {afk_since}.\
                         \nKarena: `{AFKREASON}`")
                 else:
                     await mention.reply(str(choice(AFKSTR)))
@@ -174,8 +173,7 @@ async def mention_afk(mention):
             elif mention.sender_id in USERS:
                 if USERS[mention.sender_id] % randint(2, 4) == 0:
                     if AFKREASON:
-                        await mention.reply(str(choice(AFKSTR)) f"\n\
-                            \nGue masih OFF sejak {afk_since}.\
+                        await mention.reply(f"\nGue masih OFF sejak {afk_since}.\
                             \nKarena: `{AFKREASON}`")
                     else:
                         await mention.reply(str(choice(AFKSTR)))
@@ -242,8 +240,7 @@ async def afk_on_pm(sender):
                 afk_since = f"`{int(seconds)}s` yang lalu"
             if sender.sender_id not in USERS:
                 if AFKREASON:
-                    await sender.reply(str(choice(AFKSTR)) f"\n\
-                        \nGue OFF sejak {afk_since}.\
+                    await sender.reply(f"\nGue OFF sejak {afk_since}.\
                         \nKarena: `{AFKREASON}`")
                 else:
                     await sender.reply(str(choice(AFKSTR)))
@@ -252,9 +249,8 @@ async def afk_on_pm(sender):
             elif apprv and sender.sender_id in USERS:
                 if USERS[sender.sender_id] % randint(2, 4) == 0:
                     if AFKREASON:
-                        await sender.reply(str(choice(AFKSTR)) f"\n\
-                            \nGue masih OFF sejak {afk_since}.\
-                            \nKarena: `{AFKREASON}`")
+                        await sender.reply(f"\nGue masih OFF sejak {afk_since}."
+                            f"\nKarena: `{AFKREASON}`")
                     else:
                         await sender.reply(str(choice(AFKSTR)))
                     USERS[sender.sender_id] = USERS[sender.sender_id] + 1
